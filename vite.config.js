5e2c2e43bdd5ef0540e944724034d7e3f0900de5
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2025-01-18 22:21:35 +0100

    Feat: Setting the testing

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  globals: true,
   test: {
     environment: 'jsdom',
+    setupFiles: './tests/setup.js',
   },
 })
